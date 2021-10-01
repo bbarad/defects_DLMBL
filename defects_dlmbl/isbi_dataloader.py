26adde876b015620ea45088bdf3f0ace443bd080
lukebfunk <lfunkonline@gmail.com>
2021-10-01 21:54:39 +0000

    basic isbi dataloader

--- defects_dlmbl/isbi_dataloader.py
+++ defects_dlmbl/isbi_dataloader.py
@@ -1,1 +1,26 @@
-import os
+# conda install jupyter pytorch tensorboard h5py
+
+import torch
+from torch.utils.data import Dataset
+import h5py
+
+
+class ISBI_dataloader(Dataset):
+    def __init__(self,filename):
+        self.filename = filename
+        self.samples = self.get_num_samples()
+
+    def __len__(self):
+        return self.samples
+
+    def get_num_samples(self):
+        with h5py.File(self.filename) as f:
+            samples = f['raw'].shape[0]
+        return samples
+
+    def __getitem__(self,index):
+        with h5py.File(self.filename) as f:
+            x = f['raw'][index]
+            y = f['affinities'][:,index]
+        return torch.tensor(x),torch.tensor(y)
+
