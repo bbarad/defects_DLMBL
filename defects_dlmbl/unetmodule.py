5a3c18f6be1f359d21448d119bd49568f15b5a32
Benjamin Barad <benjamin.barad@gmail.com>
2021-10-02 21:16:48 +0000

    Add convolution to UNetModule and tune

--- defects_dlmbl/unetmodule.py
+++ defects_dlmbl/unetmodule.py
@@ -4,18 +4,19 @@ from .unet import UNet
 from pytorch_lightning.core.lightning import LightningModule
 
 class UNetModule(LightningModule):
-	def __init__(self):
+	def __init__(self, num_fmaps=12, num_affinities=2):
 		super().__init__()
-
 		self.unet = UNet(in_channels=1,
-           num_fmaps=6,
-           fmap_inc_factors=2,
+           num_fmaps=num_fmaps,
+           fmap_inc_factors=3,
            downsample_factors=[[2,2],[2,2],[2,2]],
-           padding='same',
-           num_fmaps_out=2)
+           padding='same')
+		self.final_conv=torch.nn.Conv2d(num_fmaps,num_affinities, 1)
 
 	def forward(self,x):
-		return self.unet(x)
+		x= self.unet(x)
+		x=self.final_conv(x)
+		return x
 
 	def training_step(self,batch,batch_idx):
 		x,y=batch
@@ -24,10 +25,12 @@ class UNetModule(LightningModule):
 		loss=F.binary_cross_entropy_with_logits(logits,y)
 		logger = self.logger.experiment
 		self.log('train_loss',loss)
-		logger.add_image('image', x.squeeze(0))
-		logger.add_image('affinity', logits.squeeze(0))
-		logger.add_image('GT',y.squeeze(0))
+		if self.global_step % 100 == 0:
+			logger.add_image('image', x.squeeze(0))
+			affinity_image = torch.sigmoid(logits)
+			logger.add_image('affinity', affinity_image.squeeze(0))
+			logger.add_image('GT',y.squeeze(0))
 		return loss
 
 	def configure_optimizers(self):
-		return torch.optim.Adam(self.parameters(),lr=1e-3)
+		return torch.optim.Adam(self.parameters(),lr=1e-4)
