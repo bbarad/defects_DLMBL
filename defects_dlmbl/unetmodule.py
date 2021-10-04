852bf628ce8c5b799e2fb1d2ee87096363fa0ddb
lukebfunk <lfunkonline@gmail.com>
2021-10-04 17:43:30 +0000

    metric learning auxiliary task

--- defects_dlmbl/unetmodule.py
+++ defects_dlmbl/unetmodule.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import functional as F
+from .disc_loss import DiscriminativeLoss
 from .unet import UNet
 from pytorch_lightning.core.lightning import LightningModule
 from defects_dlmbl.segment_affinities import mutex_watershed
@@ -12,6 +13,7 @@ from cremi_tools.metrics import cremi_metrics
 class UNetModule(LightningModule):
 	def __init__(self, num_fmaps=18, inc_factors=3, depth = 4, offsets=[[-1,0],[0,-1], [-9, 0], [0, -9]], separating_channel=2, image_dir="images"):
 		super().__init__()
+		self.num_fmaps=num_fmaps
 		self.offsets = offsets
 		self.separating_channel=separating_channel
 		self.unet = UNet(in_channels=1,
@@ -99,3 +101,93 @@ class UNetModule(LightningModule):
 
 	def configure_optimizers(self):
 		return torch.optim.Adam(self.parameters(),lr=1e-4)
+
+class UNetModuleWithMetricAuxiliary(UNetModule):
+	def __init__(self,metric_dimensions=16,loss_alpha=0.5,**kwargs):
+		super().__init__(**kwargs)
+		self.metric_dimensions = metric_dimensions
+		self.loss_alpha = loss_alpha
+		self.output_dims = len(self.offsets)+self.metric_dimensions
+		self.final_conv=torch.nn.Conv2d(self.num_fmaps,self.output_dims, 1)
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		self.DiscriminativeLoss = DiscriminativeLoss(device)
+
+	def calculate_loss(self,logits_aff,logits_metric,gt_aff,gt_seg):
+		# affinity loss
+		py = F.sigmoid(logits_aff) 
+		loss_aff = self.DiceLoss(py, gt_aff)
+		loss_aff += len(self.offsets)
+		loss_aff /= len(self.offsets)
+		# metric loss
+		loss_metric = self.DiscriminativeLoss(logits_metric,gt_seg)
+		return (self.loss_alpha)*loss_aff + (1-self.loss_alpha)*loss_metric
+
+	def training_step(self,batch,batch_idx):
+		x,gt_aff,gt_seg=batch
+		logits=self(x)
+		crop_val = (gt_aff.shape[-1]-logits.shape[-1])/2
+		assert crop_val == int(crop_val), "Can't crop by an odd total pixel count"
+		crop_val = int(crop_val)
+		gt_aff = gt_aff[:,:,crop_val:-crop_val,crop_val:-crop_val]
+		gt_seg = gt_seg[:,:,crop_val:-crop_val,crop_val:-crop_val]
+		x = x[:,:,crop_val:-crop_val,crop_val:-crop_val]
+		gt_aff = gt_aff.float()
+
+		logits_aff = logits[:,:len(self.offsets)]
+		logits_metric = logits[:,len(self.offsets):]
+
+		assert self.metric_dimensions == logits_metric.shape[1], "logits_metric channels do not match metric_dimensions"
+
+		logits_aff *= (gt_aff!=-1).float() # ignore label -1
+
+		loss = self.calculate_loss(logits_aff,logits_metric,gt_aff,gt_seg)
+		
+		logger = self.logger.experiment
+		self.log('train_loss',loss)
+		if self.global_step % 100 == 0:
+			
+			logger.add_image('image', x[0], self.global_step)
+
+			affinity_image = torch.sigmoid(logits_aff)
+			logger.add_image('affinity', affinity_image[0], self.global_step)
+			affinity_image = affinity_image.cpu().detach().numpy()
+			segmentation = mutex_watershed(affinity_image,self.offsets,self.separating_channel,strides=None)
+
+			logger.add_image('segmentation', segmentation[0], self.global_step, dataformats='CHW')
+			logits_metric_viz = logits_metric[0,:3].detach().cpu().numpy()
+			logits_metric_viz -= logits_metric_viz.min(axis=(-2,-1),keepdims=True)
+			logits_metric_viz /= logits_metric_viz.max(axis=(-2,-1),keepdims=True)
+			logger.add_image('metric_3',logits_metric_viz, self.global_step)
+			logger.add_image('GT',gt_aff[0], self.global_step)
+			if self.global_step % 1000 == 0:
+				imsave(f'{self.image_dir}/{self.global_step}_segmentation.tif', segmentation.astype(np.uint16))
+				imsave(f'{self.image_dir}/{self.global_step}_affinity.tif', affinity_image)
+				imsave(f'{self.image_dir}/{self.global_step}_gt.tif', gt_seg[0].cpu().detach().numpy())
+				imsave(f'{self.image_dir}/{self.global_step}_image.tif', x[0].cpu().detach().numpy())
+			scores = cremi_metrics.cremi_scores(segmentation, gt_seg.cpu().detach().numpy())
+			self.log("performance",scores)
+		return loss
+		
+	def validation_step(self,batch,batch_idx):
+		x,gt_aff,gt_seg=batch
+		logits=self(x)
+		crop_val = (gt_aff.shape[-1]-logits.shape[-1])/2
+		assert crop_val == int(crop_val), "Can't crop by an odd total pixel count"
+		crop_val = int(crop_val)
+		gt_aff = gt_aff[:,:,crop_val:-crop_val,crop_val:-crop_val]
+		gt_seg = gt_seg[:,:,crop_val:-crop_val,crop_val:-crop_val]
+		gt_aff = gt_aff.float()
+
+		logits_aff = logits[:,:len(self.offsets)]
+		logits_metric = logits[:,len(self.offsets):]
+
+		logits_aff *= (gt_aff!=-1).float() # ignore label -1
+		
+		val_loss = self.calculate_loss(logits_aff,logits_metric,gt_aff,gt_seg)
+
+		affinity_image = torch.sigmoid(logits_aff).cpu().detach().numpy()
+		segmentation = mutex_watershed(affinity_image,self.offsets,self.separating_channel,strides=None)
+		val_scores = cremi_metrics.cremi_scores(segmentation, gt_seg.cpu().numpy())
+		self.log("val_loss", val_loss, prog_bar=True, on_epoch=True)
+		self.log("val_performance", val_scores)
+		return val_loss
