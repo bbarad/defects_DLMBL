289a8dc45e7a81d10f46e2f3a0d3cc029d0d2fde
sdelsig <sdelsig@gmail.com>
2021-10-04 20:45:51 +0000

    added NMJ data module

--- defects_dlmbl/datamodules.py
+++ defects_dlmbl/datamodules.py
@@ -48,3 +48,31 @@ class CREMIDataModule(LightningDataModule):
 
 	def val_dataloader(self):
 		return DataLoader(self.val,batch_size=4, num_workers=8)
+
+
+	class NMJDataModule(LightningDataModule):
+		def __init__(self, train_filename, augmenter = None, augment_and_crop=True, pad=0, offsets=[[-1, 0], [0, -1], [-9, 0], [0, -9]]):
+			super().__init__()
+			self.train_dims = None
+			self.train_filename = train_filename
+			self.augmenter = augmenter
+			self.augment_and_crop = augment_and_crop
+			self.pad = pad
+			self.offsets = offsets
+
+
+		def setup(self, stage = None):
+			full_dataset = len(CREMIDataset(self.train_filename))
+			split_index = 4*full_dataset//5
+			train_indices = list(range(split_index))
+			val_indices = list(range(split_index,full_dataset))
+			self.train = CREMIDataset(self.train_filename,indices=train_indices, augmenter=self.augmenter, offsets = self.offsets,
+									augment_and_crop=self.augment_and_crop, pad=self.pad)
+			self.val = CREMIDataset(self.train_filename,indices=val_indices, offsets = self.offsets, 
+									augment_and_crop=self.augment_and_crop, pad=self.pad)	
+
+		def train_dataloader(self):
+			return DataLoader(self.train, batch_size=4, num_workers=8, shuffle=True)
+
+		def val_dataloader(self):
+			return DataLoader(self.val, batch_size=4, num_workers=8)
